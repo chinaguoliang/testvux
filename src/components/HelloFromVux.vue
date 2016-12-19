a36a5da660cc564b640ed114ef2bdb6c7afffcab
chen <38033919@qq.com>
2016-12-20 00:21:06 +0800

    增加一些例子

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -6,16 +6,32 @@
   <group title="cell demo">
     <cell title="Vux" value="Cool" is-link></cell>
   </group>
+
+  <divider>Horizontal</divider>
+  <flexbox style="height:40px;">
+    <flexbox-item><div class="flex-demo">1</div></flexbox-item>
+    <flexbox-item><div class="flex-demo">2</div></flexbox-item>
+    <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+  </flexbox>
+  <divider>Vertical</divider>
+  <flexbox orient="vertical" :margin-left=0>
+    <flexbox-item><div class="flex-demo" style="margin-left:0">1</div></flexbox-item>
+    <flexbox-item><div class="flex-demo" style="margin-left:0">2</div></flexbox-item>
+    <flexbox-item><div class="flex-demo" style="margin-left:0">3</div></flexbox-item>
+  </flexbox>
 </template>
 
 <script>
 import Group from 'vux/src/components/group'
 import Cell from 'vux/src/components/cell'
+ //引入组件
+    import {Divider,FlexboxItem,Flexbox} from 'vux/src/components'
 
 export default {
   components: {
     Group,
-    Cell
+    Cell,
+     Divider,FlexboxItem,Flexbox
   },
   data () {
     return {
@@ -38,4 +54,13 @@ export default {
   width: 100px;
   height: 100px
 }
+
+ .flex-demo {
+      text-align: center;  /*文本居中显示*/
+      color: #EEE;
+      background-color: #0077FF;
+      margin-bottom: 16px;
+      border-radius: 6px; /*按钮的圆角弧度*/
+      -webkit-background-clip: padding-box;
+    }
 </style>
