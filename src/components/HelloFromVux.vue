3721ccd46beb34957b100299395dd1b4b2abf397
chen <38033919@qq.com>
2016-12-20 01:02:10 +0800

    增加一些别动ui

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="vux-demo">
-    <img class="logo" src="../assets/vux_logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+  <scroller lock-x scrollbar-y use-pulldown @pulldown:loading="load" enable-horizontal-swiping>
+    <div class="box2">
+      <swiper :list="list" direction="horizontal" auto :min-moving-distance="20" height="300px"></swiper>
+    </div>
+  </scroller>
   <group title="cell demo">
     <cell title="Vux" value="Cool" is-link></cell>
     <cell title="A" value="B" is-link></cell>
@@ -11,21 +12,34 @@
 
   <divider>Horizontal</divider>
   <flexbox style="height:40px;">
-    <flexbox-item><div class="flex-demo">1</div></flexbox-item>
-    <flexbox-item><div class="flex-demo">2</div></flexbox-item>
-    <flexbox-item><div class="flex-demo">3</div></flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo">1</div>
+    </flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo">2</div>
+    </flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo">3</div>
+    </flexbox-item>
   </flexbox>
   <divider>Vertical</divider>
   <flexbox orient="vertical" :margin-left=0>
-    <flexbox-item><div class="flex-demo" style="margin-left:0">1</div></flexbox-item>
-    <flexbox-item><div class="flex-demo" style="margin-left:0">2</div></flexbox-item>
-    <flexbox-item><div class="flex-demo" style="margin-left:0">3</div></flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo" style="margin-left:0">1</div>
+    </flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo" style="margin-left:0">2</div>
+    </flexbox-item>
+    <flexbox-item>
+      <div class="flex-demo" style="margin-left:0">3</div>
+    </flexbox-item>
   </flexbox>
 </template>
 
 <script>
 import Group from 'vux/src/components/group'
 import Cell from 'vux/src/components/cell'
+import { Scroller, Swiper } from 'vux/src/components'
  //引入组件
     import {Divider,FlexboxItem,Flexbox} from 'vux/src/components'
 
@@ -33,7 +47,17 @@ export default {
   components: {
     Group,
     Cell,
-     Divider,FlexboxItem,Flexbox
+     Divider,FlexboxItem,Flexbox,
+     Scroller,
+    Swiper
+  },
+  methods: {
+    load (uuid) {
+      const _this = this
+      setTimeout(function () {
+        _this.$broadcast('pulldown:reset', uuid)
+      }, 2000)
+    }
   },
   data () {
     return {
@@ -41,10 +65,24 @@ export default {
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'Hello World!'
+      msg: 'Hello World!',
+      list: [{
+        url: 'http://www.baidu.com',
+        img: 'http://pic.90sjimg.com/back_pic/qk/back_origin_pic/00/02/90/741f84aab6a1834641f571922f406116.jpg',
+        title: '如何手制一份秋意的茶？'
+      }, {
+        url: 'http://www.baidu.com',
+        img: 'http://pic.90sjimg.com/back_pic/u/00/04/28/87/55ed40935d4ac.jpg',
+        title: '茶包VS原叶茶'
+      }, {
+        url: 'http://www.baidu.com',
+        img: 'http://img15.3lian.com/2015/h1/258/d/52.jpg',
+        title: '播下茶籽，明春可发芽？'
+      }]
     }
   }
 }
+
 </script>
 
 <style>
@@ -65,4 +103,5 @@ export default {
       border-radius: 6px; /*按钮的圆角弧度*/
       -webkit-background-clip: padding-box;
     }
+
 </style>
