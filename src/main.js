f8a9a54975e8af1341fa29b0979facaad80208a0
chen <38033919@qq.com>
2016-12-20 22:36:59 +0800

    修改组件名称

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-import Home from './components/Hall'
+import Home from './components/Home'
 import VueRouter from 'vue-router'
 import Account from './components/Account'
 import UserInfo from './components/UserInfo'
