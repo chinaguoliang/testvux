785ab8f93f969b67dc0943452ee5f49bd9ac7940
guoliang.chen@21chinamall.com <guoliang.chen@21chinamall.com>
2016-12-21 15:32:16 +0800

    测试vueResource

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -10,6 +10,10 @@
     }
 </style>
 <script>
+  import VueResource from 'vue-resource';
+  import Vue from 'vue';
+  Vue.use(VueResource);
+
     export default{
         data(){
             return{
@@ -22,6 +26,18 @@
           console.log('userPassword: ' + this.$route.params.userPassword);
           this.userName = this.$route.params.userName;
           this.userPassword = this.$route.params.userPassword;
+
+          this.$http.get("http://amidgame.cn/api/attendance/getAllLocation").then(
+            function (res) {
+                // 处理成功的结果
+                //alert("success" + res.body);
+                 console.log("requestMsg:" + res);
+            },function (res) {
+            // 处理失败的结果
+                //alert("failed");
+              console.log("requestMsg:" + res);
+            }
+          );
         },
         components:{
         }
